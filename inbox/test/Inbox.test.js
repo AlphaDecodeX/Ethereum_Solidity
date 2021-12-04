79eb936f8796237aeb6164d3d37e51120d16c552
unknown <lpsk5713@gmail.com>
2021-12-05 03:31:07 +0530

    Refreshed with Mocha and Ganache

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,3 +3,30 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+// class Car {
+//     park() {
+//         return 'Stopped';
+//     }
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//     car = new Car();
+// })
+
+// describe('Car Class', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'Stopped');
+//     });
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom');
+//     })
+// });
